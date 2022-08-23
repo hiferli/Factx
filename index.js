9fb73914ffcd75a86756f1278a7de9adc1543a31
IshaanJoshi <joshi.ishaan.2001@gmail.com>
2022-08-23 15:30:54 +0530

    Hiding API Key in .gitignore

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+var key = config.API_KEY;
+
 $.ajax({
     method: 'GET',
     url: 'https://api.api-ninjas.com/v1/facts?limit=' + 3,
-    headers: { 'X-Api-Key': 'Some Random API Key'},
+    headers: { 'X-Api-Key': key},
     contentType: 'application/json',
     success: function(result) {
         console.log(result);
