b4d6331fc20dc39aca7b2b6603acc491d9146dac
IshaanJoshi <joshi.ishaan.2001@gmail.com>
2022-08-24 11:19:22 +0530

    API Calling to Append Fact.

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
 var key = config.API_KEY;
 
-function updateLimit(limit){
-    document.getElementById("limitNumber").innerText = limit;
-}
+function updateLimit(limit) {}
 
-$.ajax({
-    method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/facts?limit=' + 3,
-    headers: { 'X-Api-Key': key},
-    contentType: 'application/json',
-    success: function(result) {
-        console.log(result);
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
+// $.ajax({
+//     method: 'GET',
+//     url: 'https://api.api-ninjas.com/v1/facts?limit=' + 3,
+//     headers: { 'X-Api-Key': key},
+//     contentType: 'application/json',
+//     success: function(result) {
+//         console.log(result);
+//     },
+//     error: function ajaxError(jqXHR) {
+//         console.error('Error: ', jqXHR.responseText);
+//     }
+// });
+
+$(document).ready(function () {
+  $("#limit").change(function () {
+    var limit = $("#limit").val();
+    // console.log(limit);
+    $("#limitNumber").text(limit);
+
+    if(limit){
+      $.ajax({
+        method: "GET",
+        url: "https://api.api-ninjas.com/v1/facts?limit=" + limit,
+        headers: { "X-Api-Key": key },
+        contentType: "application/json",
+        success: function (result) {
+          $(".facts").empty();
+
+          result.forEach(fact => {
+            $(".facts").append("<p class='text-center'>" + fact["fact"] + "</p>");
+          });
+        },
+        error: function ajaxError(jqXHR) {
+          console.error("Error: ", jqXHR.responseText);
+        },
+      });
     }
+
+  });
 });
