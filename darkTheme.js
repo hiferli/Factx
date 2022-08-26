8621c781ee26f4709e87fb8bf87b4fb64b262cc6
IshaanJoshi <joshi.ishaan.2001@gmail.com>
2022-08-26 11:27:54 +0530

    Added Darkmode on Toggle (Checkbox)

--- darkTheme.js
+++ darkTheme.js
@@ -1,5 +1,9 @@
 $("#flexSwitchCheckDefault").change(function() {
     if(this.checked) {
-      console.log("Clicked")
+      // document.getElementById("mode").href="darkTheme.css";
+      $("#mode").prop("href" , "darkTheme.css");
+    } else {
+      // document.getElementById("mode").href="style.css";
+      $("#mode").prop("href" , "style.css");
     }
   });
