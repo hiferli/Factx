076efd95f11fc864b2b689da9d1cf632f5bcc6b1
IshaanJoshi <joshi.ishaan.2001@gmail.com>
2022-08-29 10:23:58 +0530

    Working on Dark Mode

--- darkTheme.js
+++ darkTheme.js
@@ -1,9 +1,12 @@
-$("#flexSwitchCheckDefault").change(function() {
-    if(this.checked) {
-      // document.getElementById("mode").href="darkTheme.css";
-      $("#mode").prop("href" , "darkTheme.css");
-    } else {
-      // document.getElementById("mode").href="style.css";
-      $("#mode").prop("href" , "style.css");
-    }
+$("#flexSwitchCheckDefault").change(function(e) {
+  if(this.checked) {
+    // document.getElementById("mode").href="darkTheme.css";
+    $("#mode").prop("href" , "darkTheme.css");
+  } else {
+    // document.getElementById("mode").href="style.css";
+    $("#mode").prop("href" , "lightTheme.css");
+  }
+  
   });
+
+  
